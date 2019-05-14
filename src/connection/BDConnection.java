3cdd53d4c323398ee6cd2eeb1a41585a0da60b4f
Todes <16521321@gm.uit.edu.vn>
2019-05-14 16:18:17 +0700

    The Third commit

--- src/connection/BDConnection.java
+++ src/connection/BDConnection.java
@@ -5,10 +5,92 @@
  */
 package connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author PC
  */
 public class BDConnection {
+    private String dbname;
+    private String userid;
+    private String userpwd;
     
+    private String host;
+    private String port;
+
+    public BDConnection() {
+        BufferedReader instream;
+        try {
+            instream = Files.newBufferedReader(Paths.get("src/textfile/databaseinfo.txt"));
+            LineNumberReader in = new LineNumberReader(instream);
+            
+            in.readLine();                          // bỏ line trạng thái đầu tiên của file
+            this.dbname = in.readLine();
+            this.userid = in.readLine();
+            this.userpwd = in.readLine();
+            this.host = in.readLine();
+            this.port = in.readLine();
+            
+            in.close();
+            instream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(BDConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public BDConnection(String dbname, String userid, String userpwd, String host, String port) {
+        this.dbname = dbname;
+        this.userid = userid;
+        this.userpwd = userpwd;
+        this.host = host;
+        this.port = port;
+    }
+    
+    
+    public Connection getCon() throws SQLException
+    {
+        Connection cn = null;
+        
+        try
+        {
+            //1. nap driver jdbc4
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            
+            //2. tao ket noi den co so du lieu
+            String url = "jdbc:sqlserver://" + host + ":" + port + "; database = " + dbname;
+            cn = DriverManager.getConnection(url, userid, userpwd);
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database: " + this.dbname + " in hostip: " + this.host + " can not connect!");
+            ex.getStackTrace();
+            
+            
+            // try another connect
+            this.dbname = "COMMACOFFEESHOP";
+            this.userid = "";
+            this.userpwd = "";
+            this.host = "1";
+            this.port = "";
+            try{
+                //1. nap driver jdbc4
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+                //2. tao ket noi den co so du lieu
+                String url = "jdbc:sqlserver://" + host + ":" + port + "; database = " + dbname;
+                cn = DriverManager.getConnection(url, userid, userpwd);
+            }catch (ClassNotFoundException ex2) {
+                System.out.println("Database: " + this.dbname + " in hostip: " + this.host + " can not connect!");
+                ex2.getStackTrace();
+            }
+        }
+        
+        return cn;
+    }
 }
--- src/entity/Admin.java
+++ src/entity/Admin.java
@@ -5,10 +5,47 @@
  */
 package entity;
 
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  *
  * @author PC
  */
 public class Admin {
+    private SimpleStringProperty adId, userName, passWord;
+
+    public SimpleStringProperty getAdId() {
+        return adId;
+    }
+
+    public void setAd_id(SimpleStringProperty ad_id) {
+        this.adId = ad_id;
+    }
+
+    public SimpleStringProperty getUserName() {
+        return userName;
+    }
+
+    public void setUserName(SimpleStringProperty userName) {
+        this.userName = userName;
+    }
+
+    public SimpleStringProperty getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(SimpleStringProperty passWord) {
+        this.passWord = passWord;
+    }
+    
+    public Admin(String id, String username, String pass) {
+        this.adId = new SimpleStringProperty(id);
+        this.userName = new SimpleStringProperty(username);
+        this.passWord = new SimpleStringProperty(pass);
+    }
+    
+    public Admin(){
+        this(null, null, null);
+    }
     
 }
